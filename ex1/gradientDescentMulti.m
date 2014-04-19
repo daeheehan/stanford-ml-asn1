4eccd00b411dff8ec4c6969c420f3d37643e4379
lethe <lethe@jokerui-iMac-2.local>
2014-04-19 17:16:28 +0900

    finished ex1 and change merge other exercises

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -1,6 +1,6 @@
-function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -9,15 +9,18 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-endfor
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
     %
     % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
+    %       of the cost function (computeCostMulti) and gradient here.
     %
 
+
+
+
+    theta -= alpha / m * X' * (X * theta - y) ;
 
 
 
@@ -27,7 +30,7 @@ endfor
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+    J_history(iter) = computeCostMulti(X, y, theta);
 
 end
 
